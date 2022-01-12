c54615bff2595eedb0d041707790169740472545
Thien <bananakind123@gmail.com>
2022-01-12 12:05:07 +0700

    fix pipe

--- src/app/pipes/already-friend.pipe.ts
+++ src/app/pipes/already-friend.pipe.ts
@@ -8,13 +8,15 @@ export class AlreadyFriendPipe implements PipeTransform {
   transform(val: any): any {
     let _id = val._id;
     let userDataFriend = val.userData.friends;
-    let result = userDataFriend.findIndex((item: any) => item.friend._id == _id);
-    if (result == 0) {
-      return 0;
-    } else if (result == -1) {
-      return -1;
+    let result = -1;
+    for (let i = 0; i < userDataFriend.length; i++) {
+      if (userDataFriend[i].friend._id == _id) {
+        result = 0;
+        break;
+      }
+
     }
-    return -1;
+    return result;
   }
 
 }

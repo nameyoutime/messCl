be1883df207cc91645729a6c70ba4438db29f3d2
Thiên Thiên <77221040+nameyoutime@users.noreply.github.com>
2022-01-30 13:00:12 +0700

    ignore config

--- server/src/config.js
+++ server/src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: '8080',
     HOST: '0.0.0.0',
-    CONNECTIONSTRING: 'mongodb+srv://admin:admin@messclone.caesv.mongodb.net/MessClone?retryWrites=true&w=majority',
+    CONNECTIONSTRING: '',
 }
